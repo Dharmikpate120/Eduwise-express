7aba7fc8d7d330057881618a17dd7309d5038b28
Dharmikpate120 <Dharmik7458@gmail.com>
2025-02-10 17:29:27 +0530

    fixing issue

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-
+const path = require("path");
 dotenv.config();
 
 app.set("view engine", "ejs");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
