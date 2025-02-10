41a3b6268efd9a5aab4100f21d17878419802f25
Dharmikpate120 <Dharmik7458@gmail.com>
2025-02-10 16:54:27 +0530

    fixing some issues

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-const indexRoutes = require('./routes/index.routes');
-const backendRoutes = require('./routes/index.backend');
+const indexRoutes = require("./routes/index.routes");
+const backendRoutes = require("./routes/index.backend");
 
-app.use('/', indexRoutes);
-app.use('/backend', backendRoutes)
+app.use("/", indexRoutes);
+app.use("/backend", backendRoutes);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
